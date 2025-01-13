3c39d2612fe100072d7452f5ed2a3c8e97aee612
Niko4308 <24122006K>
2025-01-13 13:38:55 -0800

    update inner join left join

--- 2tablets.sql
+++ 2tablets.sql
@@ -1,32 +1,49 @@
-CREATE TABLE FirstTable (
+IF OBJECT_ID('MyTable', 'U') IS NOT NULL
+    DROP TABLE MyTable;
+
+CREATE TABLE MyTable (
     ID INT PRIMARY KEY,
     Col2 INT,
-    Col3 INT,
-    Col4 INT,
-    Col5 INT
+    Col3 INT
 );
 
-INSERT INTO FirstTable (ID, Col2, Col3, Col4, Col5)
-VALUES
-(1, 10, 100, 1000, 10000),
-(2, 20, 200, 2000, 20000),
-(3, 30, 300, 3000, 30000),
-(4, 40, 400, 4000, 40000);
+INSERT INTO MyTable (ID, Col2, Col3)
+VALUES 
+    (1, 1000, 2000),
+    (2, 3000, 4000),
+    (3, 5000, 6000),
+    (4, 7000, 8000),
+    (5, 9000, 10000),
+    (6, 11000, 12000),
+    (7, 13000, 14000);
 
-CREATE TABLE SecondTable (
+IF OBJECT_ID('AnotherTable', 'U') IS NOT NULL
+    DROP TABLE AnotherTable;
+
+CREATE TABLE AnotherTable (
     ID INT PRIMARY KEY,
-    Col2 NVARCHAR(50),
-    Col3 NVARCHAR(50),
     Col4 NVARCHAR(50),
     Col5 NVARCHAR(50)
 );
 
-INSERT INTO SecondTable (ID, Col2, Col3, Col4, Col5)
+INSERT INTO AnotherTable (ID, Col4, Col5)
 VALUES
-(1, 'Текст1', 'Текст2', 'Текст3', 'Текст4'),
-(2, NULL, NULL, NULL, NULL),
-(3, NULL, NULL, NULL, NULL),
-(4, 'Текст5', 'Текст6', 'Текст7', 'Текст8'),
-(5, 'Текст9', 'Текст10', 'Текст11', 'Текст12'),
-(6, 'Текст13', 'Текст14', 'Текст15', 'Текст16'),
-(7, 'Текст17', 'Текст18', 'Текст19', 'Текст20');
+    (1, 'Текст1', 'Текст2'),
+    (2, NULL, NULL),
+    (3, NULL, NULL),
+    (4, 'Текст3', 'Текст4'),
+    (5, 'Текст5', 'Текст6'),
+    (6, 'Текст7', NULL),
+    (7, NULL, 'Текст7');
+
+SELECT 
+    MyTable.ID, MyTable.Col2, MyTable.Col3, AnotherTable.Col4, AnotherTable.Col5
+FROM MyTable
+INNER JOIN AnotherTable
+ON MyTable.ID = AnotherTable.ID;
+
+SELECT 
+    MyTable.ID, MyTable.Col2, MyTable.Col3, AnotherTable.Col4, AnotherTable.Col5
+FROM MyTable
+LEFT JOIN AnotherTable
+ON MyTable.ID = AnotherTable.ID;
